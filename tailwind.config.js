4e6ffb51615143d05a45b5c19f3243fcd3ffacbc
Akash Sharma <akashs3468@gmail.com>
2024-12-06 20:27:28 +0530

    depcheck

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+// tailwind.config.js
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {
-      fontFamily: {
-        heading: ['Dancing Script', 'cursive'],
-        body: ['Poppins', 'sans-serif'],
-      },
-    },
+    extend: {},
   },
   plugins: [],
 };
